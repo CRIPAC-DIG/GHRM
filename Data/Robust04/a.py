93253457cd622b72da5907eb42a402e601d1f982
Sherryuu <53335125+Sherryuu@users.noreply.github.com>
2021-08-16 16:30:02 +0800

    Delete Data directory

--- Data/Robust04/a.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-trec_eval_f = '../../HGCF/bin/trec_eval'
-VALIDATION_METRIC = 'ndcg_cut.20'
-#runf = 'run.robust04.bm25.topics.robust04.txt'
-for i in range(1,6):
-	runf = 'test_run/f{}.test.run'.format(str(i))
-	qrelf = 'qrels'
-	output = subprocess.check_output([trec_eval_f, '-m', VALIDATION_METRIC, qrelf, runf]).decode().rstrip()
-	print(output)
